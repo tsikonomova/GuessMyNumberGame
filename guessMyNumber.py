30c01072cfeac9946896df0aeb5fd2b3cb5bc104
tsikonomova <tsveti@protonmail.ch>
2016-05-18 19:29:14 +0200

    commit the game itself

--- guessMyNumber.py
+++ guessMyNumber.py
@@ -1,1 +1,13 @@
-# GuessMyNumberGame
+#!/usr/bin/env python 
+import random, math
+random.seed()
+x = math.floor(random.random()*100)+1
+z = 0
+b = 0
+while x != z:
+   b=b+1
+   z = input("Guess my number: ")
+   if z < x: print("Higher!")
+   if z > x: print("Lower!")
+   if z == x + 1 or z == x - 1  : print("You are very close!")
+print("Correct! " + str(b) + " tries =)")
